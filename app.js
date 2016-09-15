4ac49a6b4ba459af4c8f2c3d739926dbf4a31bd8
Plamen Neshkov <plamen@tuta.io>
2016-09-15 17:43:07 +0300

    Everything up to gulp

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
+"use strict";
+
 var express = require("express");
 
 var app = express();
 
 var port = 8080;
 
-app.get("/", function(req, res) {
-   res.send("Hello world"); 
-});
+app.use(express.static("public"));
+app.use(express.static("src/views"));
+
 
 app.get("/books", function(req, res) {
    res.send("Hello books"); 
 });
 
 app.listen(port, function(err) {
+    if (err) {
+        console.log(err);
+    }
     console.log("Running server on port " + port);
 });
